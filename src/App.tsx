ea0a80d5967355c9ebe8f11913ef73e31f67ed4f
Harsha Udara <116781547+UdaraDZoysa@users.noreply.github.com>
2025-02-17 14:34:21 +0530

    Comment usestate in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+//import { useState } from "react";
 import "./App.css";
 import Marketplace from "./components/MarketPlace";
 import Example from "./components/Example";
