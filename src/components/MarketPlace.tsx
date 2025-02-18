56c4c89cd74228bbdb47747b17cb921149c5082b
Thakshila-Prasadanee <dias12436@usci.ruh.ac.lk>
2025-02-18 20:19:24 +0530

    Create FilterSideBar

--- src/components/MarketPlace.tsx
+++ src/components/MarketPlace.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import AutoCompleteSearchBar from "./AutocompletedSearchBar";
+import AutoCompleteSearchBar from "./AutoCompleteSearchBar"; 
+import FilterSideBar from "./FilterSideBar";
+import { Sidebar } from "lucide-react";
 
 const products = [
   "iPhone 15 Pro",
@@ -25,8 +27,27 @@ const fetchProductDetails = async (query: string) => {
 
 const Marketplace: React.FC = () => {
   return (
-    <div className="flex flex-col items-center min-h-screen p-8 md:p-12">
-      <AutoCompleteSearchBar data={products} onSearch={fetchProductDetails} />
+    <div className="flex min-h-screen p-6 bg-gray-100 md:p-12">
+      {/* Sidebar (Left) */}
+      {/* <div className="w-1/4 min-w-[250px] bg-white shadow-lg rounded-lg p-4">
+        <FilterSidebar />
+      </div> */}
+
+      <FilterSideBar />
+
+      {/* Main Content (Right) */}
+      <div className="flex flex-col flex-grow ml-6">
+        {/* Search Bar */}
+        <div className="mb-6">
+          <AutoCompleteSearchBar data={products} onSearch={fetchProductDetails} />
+        </div>
+
+        {/* Product Listings (Placeholder) */}
+        <div className="p-6 bg-white rounded-lg shadow-md">
+          <h2 className="text-xl font-semibold">Product Listings</h2>
+          <p className="text-gray-600">Products will be displayed here...</p>
+        </div>
+      </div>
     </div>
   );
 };
