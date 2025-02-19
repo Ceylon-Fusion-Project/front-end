a6556684d73f405c9242b4c3540eb8936fad550e
RaviprabhaSanduniJayalal <jayalal12351@usci.ruh.ac.lk>
2025-02-19 08:49:37 +0530

    modified 2 sections

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -5,7 +5,7 @@ import communityImpact from "../assets/images/community-impact.jpg";
 
 const IntroSection = () => {
   return (
-    <div className="bg-[#FAF3E0] py-10">  {/* Updated Background */}
+    <div className="bg-[#FAF3E0] min-h-screen flex items-center py-20">  {/* Updated Background */}
       <div className="container mx-auto px-4">
         {/* Section Title */}
         <div className="text-center mb-10">
