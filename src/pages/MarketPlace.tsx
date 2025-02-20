f183fd51b8be433aaa6fce602ff13eea68d1f8dd
Thakshila-Prasadanee <dias12436@usci.ruh.ac.lk>
2025-02-20 12:27:49 +0530

    Updated

--- src/pages/MarketPlace.tsx
+++ src/pages/MarketPlace.tsx
@@ -1,66 +1,17 @@
-//import React, { useState } from "react";
-//import AutoCompleteSearchBar from "./AutoCompleteSearchBar"; 
-import FilterButton from "./../components/FilterButton";
-
-import SlideshowBanner from "./../components/SlideshowBanner";
-
-import SortBar from "./../components/SortBar"; // Import the SortButtons component
-
-
-// const products = [
-//   "iPhone 15 Pro",
-//   "Samsung Galaxy S23",
-//   "MacBook Pro M2",
-//   "Dell XPS 13",
-//   "Sony WH-1000XM5",
-//   "Apple Watch Ultra",
-//   "Google Pixel 7",
-// ];
-
-// const fetchProductDetails = async (query: string) => {
-//   try {
-//     const response = await fetch(
-//       `/api/products?name=${encodeURIComponent(query)}`
-//     );
-//     const data = await response.json();
-//     console.log("Product Details:", data);
-//   } catch (error) {
-//     console.error("Error fetching product details:", error);
-//   }
-// };
+import Navbar from "@/components/Navbar";
 
 const Marketplace: React.FC = () => {
   return (
 
-    <div>
-      <div><SlideshowBanner/></div>
-      <div className="flex min-h-screen p-6 bg-gray-100 md:p-12">
+      <div className="h-screen overflow-y-scroll text-textPrimary snap-y snap-mandatory">
+
+        {/* Navigation Bar */}
+        <section className="snap-start">
+          <Navbar />
+        </section>
         
-        {/* Sidebar (Left) */}
-        {/* <FilterSideBar /> */}
-
-        <FilterButton />
-
-        {/* Main Content (Right) */}
-        <div className="flex flex-col flex-grow ml-6">
-          {/* Search Bar */}
-          <div className="mb-6">
-{/*            <AutoCompleteSearchBar data={products} onSearch={fetchProductDetails} /> */}
-          </div>
-
-
-          {/* Sort Buttons */}
-          <div className="flex flex-wrap gap-2">
-            <SortBar />
-          </div>
-        </div>
-        {/* Product Listings (Placeholder) */}
-        <div className="p-6 bg-white rounded-lg shadow-md">
-          <h2 className="text-xl font-semibold">Product Listings</h2>
-          <p className="text-gray-600">Products will be displayed here...</p>
-        </div>
       </div>
-    </div>
+      
   );
 };
 
