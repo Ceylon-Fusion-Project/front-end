d06d753dda153a2218642363b589231e2db3a2d8
RaviprabhaSanduniJayalal <jayalal12351@usci.ruh.ac.lk>
2025-02-20 10:31:13 +0530

    modified bg

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,5 @@
 import {Routes, Route} from 'react-router-dom';
-//import LandingPage from '../LandingPage';
+///import LandingPage from '../LandingPage';
 import ProductRoutes from './ProductRoutes';
 // import FeatureXRoutes from "./FeatureXRoutes"; // Example: Other feature routes
 // import NotFoundPage from "../pages/NotFoundPage"; // Optional 404 Page
@@ -7,7 +7,7 @@ import ProductRoutes from './ProductRoutes';
 export default function AppRoutes() {
     return (
         <Routes>
-            {/* <Route path="/" element={<LandingPage />} /> */}
+            {/* <Route path="/landingpage" element={<LandingPage />} /> */}
             {/* <Route path="/feature-x" element={<FeatureXRoutes />} /> */}
             {/* <Route path="*" element={<NotFoundPage />} /> */}
             
