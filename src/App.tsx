3b0561209cf6d012b55572d4750eadac96d232c4
Harsha Udara <hudaragc@gmail.com>
2025-02-20 09:29:22 +0530

    solve check issues of refined basic routings

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
-import Navbar from "./components/Navbar";
-import HeroBanner from "./components/HeroBanner";
-import Footer from "./components/footer";
 
 function App() {
   return (
