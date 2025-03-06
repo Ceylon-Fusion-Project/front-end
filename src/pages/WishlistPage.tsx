66b53670db1280af5dc8822c001e5d2d3aaccae6
Sameera Athukorala <athukorala12376@usci.ruh.ac.lk>
2025-03-06 22:52:15 +0530

    created WishlistPage

---
+++ src/pages/WishlistPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Wishlist from "../components/Wishlistcomponents/Wishlist";
+
+const WishlistPage: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <Wishlist />
+    </div>
+  );
+};
+
+export default WishlistPage;
