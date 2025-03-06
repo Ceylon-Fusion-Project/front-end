5234d3e43b8c2bab7e8a4e4145a4f80af21447bb
Sameera Athukorala <athukorala12376@usci.ruh.ac.lk>
2025-03-06 15:09:50 +0530

    Created ShoppingCart page

---
+++ src/pages/ShoppingCart.tsx
@@ -0,0 +1,11 @@
+import Cart from "../components/ShoppingCartComponents/Cart";
+
+const ShoppingCartPage: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <Cart />
+    </div>
+  );
+};
+
+export default ShoppingCartPage;
