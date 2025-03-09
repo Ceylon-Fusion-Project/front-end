a40dcdff8d0d53af5ee4abc010f2bbf545d1432d
Harsha Udara <hudaragc@gmail.com>
2025-03-10 02:23:23 +0530

    Create basic add product form v2

--- src/components/AdminComponents/ProductForm.tsx
+++ src/components/AdminComponents/ProductForm.tsx
@@ -497,7 +497,7 @@
 // export default ProductForm;
 
 // src/components/ProductForm.tsx
-import React, { useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
